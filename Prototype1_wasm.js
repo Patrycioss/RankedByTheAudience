db167b6d229e79d5cc097ee29e58169f270b4262
Mightylight <59601297+Mightylight@users.noreply.github.com>
2024-10-23 14:56:32 +0200

    KeyboardTest10

--- Prototype1_wasm.js
+++ Prototype1_wasm.js
@@ -361,8 +361,8 @@ function createWasm() {
   };
   function receiveInstance(instance, module) {
     wasmExports = instance.exports;
-    wasmTable = wasmExports["ci"];
-    addOnInit(wasmExports["Zh"]);
+    wasmTable = wasmExports["ui"];
+    addOnInit(wasmExports["pi"]);
     removeRunDependency("wasm-instantiate");
     return wasmExports;
   }
@@ -384,14 +384,14 @@ function createWasm() {
 var tempDouble;
 var tempI64;
 var ASM_CONSTS = {
-  667680: function _() {
+  668320: function _() {
     if (navigator.userAgent.toLowerCase().indexOf("chrome") > -1) {
       console.log("%c    %c    Made with Defold    %c    %c    https://www.defold.com", "background: #fd6623; padding:5px 0; border: 5px;", "background: #272c31; color: #fafafa; padding:5px 0;", "background: #39a3e4; padding:5px 0;", "background: #ffffff; color: #000000; padding:5px 0;");
     } else {
       console.log("Made with Defold -=[ https://www.defold.com ]=-");
     }
   },
-  668108: function _($0) {
+  668748: function _($0) {
     var jsResult;
     var isSuccess = 1;
     try {
@@ -405,13 +405,13 @@ var ASM_CONSTS = {
     var stringOnWasmHeap = stringToNewUTF8(jsResult);
     return stringOnWasmHeap;
   },
-  668376: function _() {
+  669016: function _() {
     document.removeEventListener("click", Module.__defold_interaction_listener);
     document.removeEventListener("keyup", Module.__defold_interaction_listener);
     document.removeEventListener("touchend", Module.__defold_interaction_listener);
     Module.__defold_interaction_listener = undefined;
   },
-  668664: function _() {
+  669304: function _() {
     Module.__defold_interaction_listener = function () {
       _dmScript_RunInteractionCallback();
     };
@@ -419,10 +419,10 @@ var ASM_CONSTS = {
     document.addEventListener("keyup", Module.__defold_interaction_listener);
     document.addEventListener("touchend", Module.__defold_interaction_listener);
   },
-  668985: function _($0) {
+  669625: function _($0) {
     Module.printErr(UTF8ToString($0));
   },
-  669024: function _($0) {
+  669664: function _($0) {
     Module.print(UTF8ToString($0));
   }
 };
@@ -492,6 +492,180 @@ function setValue(ptr, value) {
       abort("invalid type for setValue: ".concat(type));
   }
 }
+var wasmTableMirror = [];
+var wasmTable;
+var getWasmTableEntry = function getWasmTableEntry(funcPtr) {
+  var func = wasmTableMirror[funcPtr];
+  if (!func) {
+    if (funcPtr >= wasmTableMirror.length) wasmTableMirror.length = funcPtr + 1;
+    wasmTableMirror[funcPtr] = func = wasmTable.get(funcPtr);
+  }
+  return func;
+};
+var TextInput = {
+  _incrId: 0,
+  list: {},
+  Event: {
+    EVENT_ON_SUBMIT: 0,
+    EVENT_ON_TEXT_CHANGED: 1,
+    EVENT_ON_FOCUS_CHANGE: 2
+  },
+  ReturnKeyType: {
+    RETURN_KEY_TYPE_DONE: 0,
+    RETURN_KEY_TYPE_GO: 1,
+    RETURN_KEY_TYPE_NEXT: 2,
+    RETURN_KEY_TYPE_SEARCH: 3,
+    RETURN_KEY_TYPE_SEND: 4
+  },
+  Capitalize: {
+    CAPITALIZE_NONE: 0,
+    CAPITALIZE_SENTENCES: 1,
+    CAPITALIZE_WORDS: 2,
+    CAPITALIZE_CHARACTERS: 3
+  },
+  KeyboardType: {
+    KEYBOARD_TYPE_DEFAULT: 0,
+    KEYBOARD_TYPE_NUMBER_PAD: 1,
+    KEYBOARD_TYPE_EMAIL: 2,
+    KEYBOARD_TYPE_PASSWORD: 3
+  },
+  addEventToQueue: function addEventToQueue(id, name, value) {
+    if (TextInput.list[id]) {
+      var data = TextInput.list[id];
+      var AddToCommandQueue = TextInput._AddToCommandQueue;
+      var cstr = stringToNewUTF8(value);
+      getWasmTableEntry(AddToCommandQueue)(id, name, cstr);
+      _free(cstr);
+    }
+  },
+  isMobile: function isMobile() {
+    var check = false;
+    (function (a) {
+      if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true;
+    })(navigator.userAgent || navigator.vendor || window.opera);
+    return check;
+  }
+};
+function _JS_TextInput_clearFocus(id) {
+  if (TextInput.list[id]) {
+    var data = TextInput.list[id];
+    data.field.blur();
+  }
+}
+function _JS_TextInput_create(isHidden) {
+  var id = TextInput._incrId++;
+  var field = document.createElement("input");
+  field.setAttribute("id", "LibraryTextInput-" + id);
+  field.type = "text";
+  field.style.position = "absolute";
+  field.style.left = 0;
+  field.style.top = 0;
+  field.style.width = 0;
+  field.style.height = 0;
+  field.style.padding = 0;
+  field.style.outline = "none";
+  field.style.border = "none";
+  field.style.background = "transparent";
+  field.style.display = "block";
+  field.addEventListener("keyup", function (ev) {
+    var key = ev.code || ev.keyCode;
+    if (key === "Enter" || key === 13) {
+      TextInput.addEventToQueue(id, TextInput.Event.EVENT_ON_SUBMIT, this.value);
+    }
+  });
+  field.addEventListener("input", function (ev) {
+    TextInput.addEventToQueue(id, TextInput.Event.EVENT_ON_TEXT_CHANGED, this.value);
+  });
+  field.addEventListener("focus", function (ev) {
+    TextInput.addEventToQueue(id, TextInput.Event.EVENT_ON_FOCUS_CHANGE, "1");
+  });
+  field.addEventListener("blur", function (ev) {
+    TextInput.addEventToQueue(id, TextInput.Event.EVENT_ON_FOCUS_CHANGE, "0");
+  });
+  TextInput.list[id] = {
+    isHidden: isHidden,
+    field: field
+  };
+  var container = document.getElementById("canvas-container");
+  container.appendChild(field);
+  if (!isHidden) {
+    var styleSheet = document.createElement("style");
+    styleSheet.textContent = "#LibraryTextInput-" + id + "::placeholder { color: var(--hintColor); }";
+    document.head.appendChild(styleSheet);
+    TextInput.list[id].styleSheet = styleSheet;
+  }
+  return id;
+}
+function _JS_TextInput_focus(id) {
+  if (TextInput.list[id]) {
+    var data = TextInput.list[id];
+    if (TextInput.isMobile()) {
+      if ("virtualKeyboard" in navigator && window.location.protocol == "https:") {
+        data.field.focus();
+        navigator.virtualKeyboard.show();
+      } else {}
+    } else {
+      data.field.focus();
+    }
+  }
+}
+function _JS_TextInput_getText(id) {
+  if (TextInput.list[id]) {
+    var data = TextInput.list[id];
+    if (data.cstr_text) _free(data.cstr_text);
+    var cstr = stringToNewUTF8(data.field.value);
+    data.cstr_text = cstr;
+    return cstr;
+  }
+}
+function _JS_TextInput_initialize(c_AddToCommandQueue) {
+  TextInput._AddToCommandQueue = c_AddToCommandQueue;
+  if (TextInput.isMobile() && !("virtualKeyboard" in navigator && window.location.protocol == "https:")) {
+    var touchstartEvent = null;
+    var container = document.getElementById("canvas-container");
+    container.addEventListener("touchstart", function (ev) {
+      var _ev$originalEvent;
+      touchstartEvent = (_ev$originalEvent = ev.originalEvent) !== null && _ev$originalEvent !== void 0 ? _ev$originalEvent : ev;
+    });
+    container.addEventListener("touchend", function (ev) {
+      var touch = touchstartEvent.touches[0];
+      for (var id in TextInput.list) {
+        var data = TextInput.list[id];
+        if (data.isHidden && data.position && data.size) {
+          if (touch.pageX >= data.position.x && touch.pageY >= data.position.y && touch.pageX <= data.position.x + data.size.width && touch.pageY <= data.position.y + data.size.height) {
+            data.field.focus();
+            break;
+          }
+        }
+      }
+    });
+  }
+}
+function _JS_TextInput_remove(id) {
+  if (TextInput.list[id]) {
+    var data = TextInput.list[id];
+    if (data.cstr_text) _free(data.cstr_text);
+    data.field.parentNode.removeChild(data.field);
+    if (data.styleSheet) {
+      data.styleSheet.parentNode.removeChild(data.styleSheet);
+    }
+    delete TextInput.list[id];
+  }
+}
+function _JS_TextInput_setAutoCapitalize(id, autoCapitalize) {
+  if (TextInput.list[id]) {
+    var data = TextInput.list[id];
+    if (autoCapitalize == TextInput.Capitalize.CAPITALIZE_SENTENCES) {
+      data.field.autocapitalize = "sentences";
+    } else if (autoCapitalize == TextInput.Capitalize.CAPITALIZE_WORDS) {
+      data.field.autocapitalize = "words";
+    } else if (autoCapitalize == TextInput.Capitalize.CAPITALIZE_CHARACTERS) {
+      data.field.autocapitalize = "characters";
+    } else {
+      data.field.autocapitalize = "none";
+    }
+  }
+}
 var UTF8Decoder = typeof TextDecoder != "undefined" ? new TextDecoder("utf8") : undefined;
 var UTF8ArrayToString = function UTF8ArrayToString(heapOrArray, idx, maxBytesToRead) {
   var endIdx = idx + maxBytesToRead;
@@ -530,6 +704,106 @@ var UTF8ArrayToString = function UTF8ArrayToString(heapOrArray, idx, maxBytesToR
 var UTF8ToString = function UTF8ToString(ptr, maxBytesToRead) {
   return ptr ? UTF8ArrayToString(HEAPU8, ptr, maxBytesToRead) : "";
 };
+function _JS_TextInput_setHint(id, hint) {
+  if (TextInput.list[id] && !TextInput.list[id].isHidden) {
+    var data = TextInput.list[id];
+    data.field.placeholder = UTF8ToString(hint);
+  }
+}
+function _JS_TextInput_setHintTextColor(id, color) {
+  if (TextInput.list[id] && !TextInput.list[id].isHidden) {
+    var data = TextInput.list[id];
+    data.field.style.setProperty("--hintColor", UTF8ToString(color));
+  }
+}
+function _JS_TextInput_setKeyboardType(id, keyboardType) {
+  if (TextInput.list[id]) {
+    var data = TextInput.list[id];
+    if (keyboardType == TextInput.KeyboardType.KEYBOARD_TYPE_NUMBER_PAD) {
+      data.field.type = "number";
+    } else if (keyboardType == TextInput.KeyboardType.KEYBOARD_TYPE_EMAIL) {
+      data.field.type = "email";
+    } else if (keyboardType == TextInput.KeyboardType.KEYBOARD_TYPE_PASSWORD) {
+      data.field.type = "password";
+    } else {
+      data.field.type = "text";
+    }
+  }
+}
+function _JS_TextInput_setMaxLength(id, maxLength) {
+  if (TextInput.list[id]) {
+    var data = TextInput.list[id];
+    data.field.setAttribute("maxlength", maxLength);
+  }
+}
+function _JS_TextInput_setPosition(id, x, y) {
+  if (TextInput.list[id]) {
+    var data = TextInput.list[id];
+    if (!data.isHidden) {
+      data.field.style.left = x + "px";
+      data.field.style.top = y + "px";
+    } else {
+      data.position = {
+        x: x,
+        y: y
+      };
+    }
+  }
+}
+function _JS_TextInput_setReturnKeyType(id, returnKeyType) {
+  if (TextInput.list[id]) {
+    var data = TextInput.list[id];
+    if (returnKeyType == TextInput.ReturnKeyType.RETURN_KEY_TYPE_GO) {
+      data.field.enterkeyhint = "go";
+    } else if (returnKeyType == TextInput.ReturnKeyType.RETURN_KEY_TYPE_NEXT) {
+      data.field.enterkeyhint = "next";
+    } else if (returnKeyType == TextInput.ReturnKeyType.RETURN_KEY_TYPE_SEARCH) {
+      data.field.enterkeyhint = "search";
+    } else if (returnKeyType == TextInput.ReturnKeyType.RETURN_KEY_TYPE_SEND) {
+      data.field.enterkeyhint = "send";
+    } else {
+      data.field.enterkeyhint = "done";
+    }
+  }
+}
+function _JS_TextInput_setSize(id, width, height) {
+  if (TextInput.list[id]) {
+    var data = TextInput.list[id];
+    if (!data.isHidden) {
+      data.field.style.width = width + "px";
+      data.field.style.height = height + "px";
+    } else {
+      data.size = {
+        width: width,
+        height: height
+      };
+    }
+  }
+}
+function _JS_TextInput_setText(id, text) {
+  if (TextInput.list[id]) {
+    var data = TextInput.list[id];
+    data.field.value = UTF8ToString(text);
+  }
+}
+function _JS_TextInput_setTextColor(id, color) {
+  if (TextInput.list[id] && !TextInput.list[id].isHidden) {
+    var data = TextInput.list[id];
+    data.field.style.color = UTF8ToString(color);
+  }
+}
+function _JS_TextInput_setTextSize(id, size) {
+  if (TextInput.list[id] && !TextInput.list[id].isHidden) {
+    var data = TextInput.list[id];
+    data.field.style.fontSize = size + "px";
+  }
+}
+function _JS_TextInput_setVisible(id, visible) {
+  if (TextInput.list[id]) {
+    var data = TextInput.list[id];
+    data.field.style.display = visible ? "block" : "none";
+  }
+}
 var ___assert_fail = function ___assert_fail(condition, filename, line, func) {
   abort("Assertion failed: ".concat(UTF8ToString(condition), ", at: ") + [filename ? UTF8ToString(filename) : "unknown filename", line, func ? UTF8ToString(func) : "unknown function"]);
 };
@@ -4593,16 +4867,6 @@ function _dmDeviceJSQueue(id, samples, sample_count) {
 function _dmGetDeviceSampleRate(id) {
   return window._dmJSDeviceShared.devices[id].sampleRate;
 }
-var wasmTableMirror = [];
-var wasmTable;
-var getWasmTableEntry = function getWasmTableEntry(funcPtr) {
-  var func = wasmTableMirror[funcPtr];
-  if (!func) {
-    if (funcPtr >= wasmTableMirror.length) wasmTableMirror.length = funcPtr + 1;
-    wasmTableMirror[funcPtr] = func = wasmTable.get(funcPtr);
-  }
-  return func;
-};
 function _dmScriptHttpRequestAsync(method, url, headers, arg, onload, onerror, onprogress, send_data, send_data_length, timeout) {
   var xhr = new XMLHttpRequest();
   function listener() {
@@ -9541,6 +9805,24 @@ for (var i = 0; i < 288; ++i) {
   miniTempWebGLIntBuffers[i] = miniTempWebGLIntBuffersStorage.subarray(0, i + 1);
 }
 var wasmImports = {
+  oi: _JS_TextInput_clearFocus,
+  ni: _JS_TextInput_create,
+  mi: _JS_TextInput_focus,
+  li: _JS_TextInput_getText,
+  ki: _JS_TextInput_initialize,
+  ji: _JS_TextInput_remove,
+  ii: _JS_TextInput_setAutoCapitalize,
+  hi: _JS_TextInput_setHint,
+  gi: _JS_TextInput_setHintTextColor,
+  fi: _JS_TextInput_setKeyboardType,
+  ei: _JS_TextInput_setMaxLength,
+  di: _JS_TextInput_setPosition,
+  ci: _JS_TextInput_setReturnKeyType,
+  bi: _JS_TextInput_setSize,
+  ai: _JS_TextInput_setText,
+  $h: _JS_TextInput_setTextColor,
+  _h: _JS_TextInput_setTextSize,
+  Zh: _JS_TextInput_setVisible,
   b: ___assert_fail,
   Yh: ___syscall__newselect,
   Xh: ___syscall_accept4,
@@ -10026,58 +10308,58 @@ var wasmImports = {
 };
 var wasmExports = createWasm();
 var _wasm_call_ctors = function ___wasm_call_ctors() {
-  return (_wasm_call_ctors = wasmExports["Zh"])();
+  return (_wasm_call_ctors = wasmExports["pi"])();
 };
 var _dmExportedSymbols = Module["_dmExportedSymbols"] = function () {
-  return (_dmExportedSymbols = Module["_dmExportedSymbols"] = wasmExports["_h"])();
+  return (_dmExportedSymbols = Module["_dmExportedSymbols"] = wasmExports["qi"])();
 };
 var _main = Module["_main"] = function (a0, a1) {
-  return (_main = Module["_main"] = wasmExports["$h"])(a0, a1);
+  return (_main = Module["_main"] = wasmExports["ri"])(a0, a1);
 };
 var _free = Module["_free"] = function (a0) {
-  return (_free = Module["_free"] = wasmExports["ai"])(a0);
+  return (_free = Module["_free"] = wasmExports["si"])(a0);
 };
 var _malloc = Module["_malloc"] = function (a0) {
-  return (_malloc = Module["_malloc"] = wasmExports["bi"])(a0);
+  return (_malloc = Module["_malloc"] = wasmExports["ti"])(a0);
 };
 var _htonl2 = function _htonl(a0) {
-  return (_htonl2 = wasmExports["di"])(a0);
+  return (_htonl2 = wasmExports["vi"])(a0);
 };
 var _dmScript_Html5ReportOperationSuccess = Module["_dmScript_Html5ReportOperationSuccess"] = function (a0) {
-  return (_dmScript_Html5ReportOperationSuccess = Module["_dmScript_Html5ReportOperationSuccess"] = wasmExports["ei"])(a0);
+  return (_dmScript_Html5ReportOperationSuccess = Module["_dmScript_Html5ReportOperationSuccess"] = wasmExports["wi"])(a0);
 };
 var _dmScript_RunInteractionCallback = Module["_dmScript_RunInteractionCallback"] = function () {
-  return (_dmScript_RunInteractionCallback = Module["_dmScript_RunInteractionCallback"] = wasmExports["fi"])();
+  return (_dmScript_RunInteractionCallback = Module["_dmScript_RunInteractionCallback"] = wasmExports["xi"])();
 };
 var _setTempRet = function setTempRet0(a0) {
-  return (_setTempRet = wasmExports["gi"])(a0);
+  return (_setTempRet = wasmExports["yi"])(a0);
 };
 var _htons2 = function _htons(a0) {
-  return (_htons2 = wasmExports["hi"])(a0);
+  return (_htons2 = wasmExports["zi"])(a0);
 };
 var _ntohs2 = function _ntohs(a0) {
-  return (_ntohs2 = wasmExports["ii"])(a0);
+  return (_ntohs2 = wasmExports["Ai"])(a0);
 };
 var _JSWriteDump = Module["_JSWriteDump"] = function (a0) {
-  return (_JSWriteDump = Module["_JSWriteDump"] = wasmExports["ji"])(a0);
+  return (_JSWriteDump = Module["_JSWriteDump"] = wasmExports["Bi"])(a0);
 };
 var _setThrew2 = function _setThrew(a0, a1) {
-  return (_setThrew2 = wasmExports["ki"])(a0, a1);
+  return (_setThrew2 = wasmExports["Ci"])(a0, a1);
 };
 var _stackSave = function stackSave() {
-  return (_stackSave = wasmExports["li"])();
+  return (_stackSave = wasmExports["Di"])();
 };
 var _stackRestore = function stackRestore(a0) {
-  return (_stackRestore = wasmExports["mi"])(a0);
+  return (_stackRestore = wasmExports["Ei"])(a0);
 };
 var _stackAlloc = function stackAlloc(a0) {
-  return (_stackAlloc = wasmExports["ni"])(a0);
+  return (_stackAlloc = wasmExports["Fi"])(a0);
 };
 var dynCall_jii = Module["dynCall_jii"] = function (a0, a1, a2) {
-  return (dynCall_jii = Module["dynCall_jii"] = wasmExports["oi"])(a0, a1, a2);
+  return (dynCall_jii = Module["dynCall_jii"] = wasmExports["Gi"])(a0, a1, a2);
 };
 var dynCall_ji = Module["dynCall_ji"] = function (a0, a1) {
-  return (dynCall_ji = Module["dynCall_ji"] = wasmExports["pi"])(a0, a1);
+  return (dynCall_ji = Module["dynCall_ji"] = wasmExports["Hi"])(a0, a1);
 };
 function invoke_vii(index, a1, a2) {
   var sp = _stackSave();
